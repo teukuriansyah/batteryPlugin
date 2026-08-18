5a840b70abf0c862948f6e811914af8752f42293
riansyah24 <jackrian72@gmail.com>
2026-08-18 18:38:14 +0700

    feat: add battery level

--- android/src/main/java/com/testing/plugin/battery/batteryPlugin.java
+++ android/src/main/java/com/testing/plugin/battery/batteryPlugin.java
@@ -4,17 +4,27 @@ import com.getcapacitor.JSObject;
 import com.getcapacitor.Plugin;
 import com.getcapacitor.PluginCall;
 import com.getcapacitor.PluginMethod;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.BatteryManager;
 import com.getcapacitor.annotation.CapacitorPlugin;
 
 @CapacitorPlugin(name = "battery")
 public class batteryPlugin extends Plugin {
 
-    private battery implementation = new battery();
-
+    Context context = getContext();
+    IntentFilter ifilter = new IntentFilter(Intent.ACTION_BATTERY_CHANGED);
+    Intent batteryStatus = context.registerReceiver(null, ifilter);
+  
     @PluginMethod
     public void getBatteryLevel(PluginCall call) {
+       int level = batteryStatus.getIntExtra(BatteryManager.EXTRA_LEVEL, -1);
+       int scale = batteryStatus.getIntExtra(BatteryManager.EXTRA_SCALE, -1);
+       float batteryPct = level * 100 / (float)scale;
+      
       JSObject ret = new JSObject();
-      ret.put("level",100);
+      ret.put("level",batteryPct);
       call.resolve(ret);
     }
   
