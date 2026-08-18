53902f61c78dbbe6cb9f8c38c7467c3d1e0b49f4
riansyah24 <jackrian72@gmail.com>
2026-08-18 19:02:16 +0700

    refactor: fix code

--- android/src/main/java/com/testing/plugin/battery/batteryPlugin.java
+++ android/src/main/java/com/testing/plugin/battery/batteryPlugin.java
@@ -12,13 +12,13 @@ import com.getcapacitor.annotation.CapacitorPlugin;
 
 @CapacitorPlugin(name = "battery")
 public class batteryPlugin extends Plugin {
-
-    Context context = getContext();
-    IntentFilter ifilter = new IntentFilter(Intent.ACTION_BATTERY_CHANGED);
-    Intent batteryStatus = context.registerReceiver(null, ifilter);
   
     @PluginMethod
     public void getBatteryLevel(PluginCall call) {
+       Context context = getContext();
+    IntentFilter ifilter = new IntentFilter(Intent.ACTION_BATTERY_CHANGED);
+       Intent batteryStatus = context.registerReceiver(null, ifilter);
+      
        int level = batteryStatus.getIntExtra(BatteryManager.EXTRA_LEVEL, -1);
        int scale = batteryStatus.getIntExtra(BatteryManager.EXTRA_SCALE, -1);
        float batteryPct = level * 100 / (float)scale;
